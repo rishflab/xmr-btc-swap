6a1546f510e3acf2a110a9dc39a504436e895b67
rishflab <rishflab@hotmail.com>
2020-09-30 14:53:12 +1000

    Add genawait based stream example demonstrating how to drive alice and bob nodes

--- xmr-btc/src/alice/node.rs
+++ xmr-btc/src/alice/node.rs
@@ -1,49 +1,84 @@
 use crate::{alice, bob, SendReceive, Transport};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use futures::{
+use genawaiter::sync::{Co, Gen};
+use std::{
+    future::Future,
+    future,
+    pin::Pin,
     task::{Context, Poll},
-    Stream,
+    time::Duration,
 };
-use std::pin::Pin;
-
+use tokio::stream::Stream;
 #[derive(Debug)]
 pub struct AliceNode {
     transport: Transport<alice::Message, bob::Message>,
     state: alice::State,
 }
+use genawaiter::{yield_, GeneratorState};
 
-#[async_trait]
-impl SendReceive<alice::Message, bob::Message> for Transport<alice::Message, bob::Message> {
-    async fn send_message(&mut self, message: alice::Message) -> Result<()> {
-        let _ = self
-            .sender
-            .send(message)
-            .await
-            .map_err(|_| anyhow!("failed to send message"))?;
-        Ok(())
-    }
-
-    async fn receive_message(&mut self) -> Result<bob::Message> {
-        let message = self
-            .receiver
-            .next()
-            .await
-            .ok_or_else(|| anyhow!("failed to receive message"))?;
-        Ok(message)
-    }
+async fn async_one() -> i32 {
+    1
+}
+async fn async_two() -> i32 {
+    2
 }
 
-impl Stream for AliceNode {
-    type Item = alice::State;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match self.state {
-            alice::State::State0(..) => {
-                let a = self.transport.receive();
-                Poll::Pending
-            }
-            _ => Poll::Ready(None),
+pub fn run_even() -> Gen<i32, (), impl Future<Output = ()>> {
+    let a = Gen::new(|co| async move {
+        let mut n = async_two().await;
+        while n < 100 {
+            co.yield_(n).await;
+            n += 2;
         }
+    });
+    a
+}
+
+pub async fn run_even_until(even_number: i32) -> Result<i32> {
+    let mut even = run_even();
+    while let GeneratorState::Yielded(i)  = even.async_resume().await {
+        if i == even_number {
+            return Ok(i)
+        } else {
+            tracing::info!("{}", i);
+        }
+    }
+    return Err(anyhow!("WEfw"))
+}
+
+
+pub fn run_odd() -> Gen<i32, (), impl Future<Output = ()>> {
+    let a = Gen::new(|co| async move {
+        let mut n = async_one().await;
+        while n < 100 {
+            co.yield_(n).await;
+            n += 2;
+        }
+    });
+    a
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use tracing_subscriber::util::SubscriberInitExt;
+    use genawaiter::GeneratorState;
+    use crate::alice::node::run_even_until;
+
+    #[tokio::test]
+    async fn gen() {
+
+        let _guard = tracing_subscriber::fmt()
+            .with_env_filter("info")
+            .set_default();
+
+        let mut even = super::run_even();
+        let mut odd = super::run_odd();
+
+     
+
+        futures::future::join(run_even_until(16), run_even_until(10)).await;
     }
 }
