d26e2bc01ea053783cd02cefb9c56e9e67fe04ac
Franck Royer <franck@royer.one>
2020-11-16 16:28:18 +1100

    Fix compilation errors

--- swap/src/bin/simple_swap.rs
+++ swap/src/bin/simple_swap.rs
@@ -1,25 +1,3 @@
-use anyhow::Result;
-use swap::storage::Database;
-
-use swap::{
-    alice::{abort, simple_swap, AliceState},
-    cli::Options,
-};
-
 fn main() {
-    let opt = Options::from_args();
-
-    let io: Io = {
-        let db = Database::open(std::path::Path::new("./.swap-db/")).unwrap();
-        unimplemented!()
-    };
-
-    match opt {
-        Options::Alice { .. } => simple_swap(AliceState::Started, io),
-        Options::Recover { .. } => {
-            let stored_state: AliceState = unimplemented!("io.get_state(uuid)?");
-            abort(stored_state, io);
-        }
-        _ => {}
-    };
+    todo!()
 }
--- swap/src/io.rs
+++ swap/src/io.rs
@@ -1,3 +1,4 @@
+// TODO: Looks like our old facade, let's be sure we learn from comit-rs
 // This struct contains all the I/O required to execute a swap
 pub struct Io {
     // swarm: libp2p::Swarm<>,
